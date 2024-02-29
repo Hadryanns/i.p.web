13a82349b0251c2cd3707457840dd648786b3969
Hadryanns <hadryan2007nascimentosantos@gmail.com>
2024-02-29 00:48:29 -0300

    Aprimoramento de página_01

---
+++ script.js
@@ -0,0 +1,15 @@
+// import Swal from 'sweetalert2'
+
+// or via CommonJS
+// const Swal = require('sweetalert2')
+function TrocarPag(caminho) {
+    window.location.href=caminho
+}
+function Andamento() {
+    alert("Em andamento, quando chegar a atividade 2, eu boto")
+    // Swal.fire({
+    //     title: "Good job!",
+    //     text: "You clicked the button!",
+    //     icon: "success"
+    //   });
+}
