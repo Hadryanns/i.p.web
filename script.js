59a614bfb0e065ba4661e1a01e449942ce718c38
Hadryanns <138410769+Hadryanns@users.noreply.github.com>
2024-03-07 11:07:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,11 +5,11 @@
 function TrocarPag(caminho) {
     window.location.href=caminho
 }
-function Andamento() {
-    alert("Em andamento, quando chegar a atividade 2, eu boto")
+// function Andamento() {
+    // alert("Em andamento, quando chegar a atividade 2, eu boto")
     // Swal.fire({
     //     title: "Good job!",
     //     text: "You clicked the button!",
     //     icon: "success"
     //   });
-}
+// }
