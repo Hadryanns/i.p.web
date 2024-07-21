92de2556d0e5ef3f1d1d5e73cc9453aef23facb3
Hadryanns <hadryan2007nascimentosantos@gmail.com>
2024-07-21 17:53:09 -0300

    feat:Muda index e cria atividade 6

--- script.js
+++ script.js
@@ -1,6 +1,81 @@
 function TrocarPag(caminho) {
     window.location.href = caminho
 }
-function Alertar() {
-    alert("N tem atividade ainda, quando tiver eu enfio.")
+function splitArray(array, size) {
+    const result = [];
+    for (let i = 0; i < array.length; i += size) {
+        result.push(array.slice(i, i + size));
+    }
+    return result;
+}
+function closePopup() {
+    const popup = document.getElementById('popup');
+    popup.style.display = 'none'
+    popup.innerHTML = '<button onclick="closePopup()" class="close-popup" id="closePopup">&times;</button>'
+    document.getElementById('overlay').style.display = 'none';
+}
+function openPopup(tipo) {
+    document.getElementById('popup').style.display = 'flex';
+    contentPopup(tipo)
+    document.getElementById('overlay').style.display = 'block';
+}
+const atividades = [
+    "01/home.html",
+    "02/Home2.html",
+    "03/home3.html",
+    "04/login.html",
+    "05/Teste_css.html",
+    "06/index.html",
+]
+const projetos = [
+    "Projeto 1/jmviagens.html"
+]
+const trabalhos = [
+    "Trabalho 1/Homepage_css.html"
+]
+function contentPopup(tipo) {
+    if (tipo == "Atividades") {
+        const elementos = contPopupMaker(atividades, tipo)
+        elementos.forEach(element => {
+            document.getElementById('popup').appendChild(element)
+        });
+    }
+    else if (tipo == "Projetos") {
+        const elementos = contPopupMaker(projetos, tipo)
+        elementos.forEach(element => {
+            document.getElementById('popup').appendChild(element)
+        });
+    }
+    else if (tipo == "Trabalhos") {
+        const elementos = contPopupMaker(trabalhos, tipo)
+        elementos.forEach(element => {
+            document.getElementById('popup').appendChild(element)
+        });
+    }
+}
+function contPopupMaker(lista, tipo) {
+    const totalLinks = []
+    for (let i = 0; i < lista.length; i++) {
+        const a = document.createElement("a")
+        a.textContent = `${tipo} ${i + 1}`;
+
+        const divLink = document.createElement("div")
+        divLink.classList.add("div-link")
+        divLink.appendChild(a)
+        divLink.addEventListener("click", function () {
+            window.location.href = lista[i]
+        })
+        totalLinks.push(divLink)
+    }
+    const splitResults = splitArray(totalLinks, 6)
+    const links = []
+    for (let j = 0; j < splitResults.length; j++) {
+        const div = document.createElement("div")
+
+        for (let l = 0; l < splitResults[j].length; l++) {
+            div.appendChild(splitResults[j][l])
+        }
+        links.push(div)
+    }
+    return links
 }
